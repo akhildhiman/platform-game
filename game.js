117dbf836d3cc3f8caf46d5a725e8839dc02bbd6
akhildhiman <akhilblackmetal69@gmail.com>
2020-08-11 21:02:16 +0530

    add level array and level class

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+// creates a new vector object with x and y coordinates
 class Vec {
   constructor(x, y) {
     this.x = x
@@ -12,3 +13,47 @@ class Vec {
     return new Vec(this.x * factor, this.y * factor)
   }
 }
+
+const simpleLevelPlan = `
+......................
+..#................#..
+..#..............=.#..
+..#.........o.o....#..
+..#.@......#####...#..
+..#####............#..
+......#++++++++++++#..
+......##############..
+......................`
+
+
+const levelChars = {
+  ".": "empty", "#": "wall", "+": "lava",
+  "@": Player, "o": Coin,
+  "=": Lava, "|": Lava, "v": Lava
+}
+
+
+class Level {
+  constructor(plan) {
+    let rows = plan.trim().split("\n").map(l => [...l])
+    this.height = height
+    this.width = rows[0].length
+    this.startActors = []
+
+    this.rows = rows.map((row, y) => {
+    return row.map((ch, x) => {
+      const type = levelChars[ch]
+      if (typeof type === "string") return type
+      this.startActors.push(
+        type.create(new Vec(x, y), ch) // create a new actor
+        )
+        return empty
+      })
+   })
+  }
+}
+
+
+
+
+
